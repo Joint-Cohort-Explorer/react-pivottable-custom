638f19569bf2672c8451e1fc5c095bdbedca24df
ibarral18 <ivanbarral18@gmail.com>
2021-04-08 17:22:22 -0400

    minor changes in styling

--- src/PivotTableUI.jsx
+++ src/PivotTableUI.jsx
@@ -274,7 +274,7 @@ DraggableAttribute.propTypes = {
 class SearchButton extends React.PureComponent{
   render(){
     const size = this.props.size || "30px"
-    return(<svg width={size} height={size} viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    return(<svg width={size} height={size} viewBox="0 0 16 16" className="icon-search bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
     <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
   </svg>)
